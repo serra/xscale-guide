523bc223badd013c9effd9213fa367e596978f5a
Marijn van der Zee <marijn@serraict.com>
2019-03-18 16:58:26 +0100

    Move imported tiddlers to the _generated directory

--- import_tiddlers.py
+++ import_tiddlers.py
@@ -8,4 +8,4 @@ predicate = lambda t: 'guide' in t.tags
 guide_tiddlers = list(tw5.finditer(predicate))
 
 for tiddler in guide_tiddlers:
-    tiddler.export_to_file('./_wiki/' + tiddler.title + '.rst')
+    tiddler.export_to_file('./source/_generated/' + tiddler.title + '.rst')
